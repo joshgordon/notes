6830467ada09983f95e08f82ab5e6d5524571726
Josh Gordon <github@joshgordon.net>
2013-04-05 18:38:15 -0400

    Finished script (for real.)

--- mkIndex.sh
+++ mkIndex.sh
@@ -2,8 +2,10 @@
 
 mdHead="<html><head><link href="http://notes.joshgordon.net/style.css" rel="stylesheet"></link><title>Directory Listing</title></head><body>" 
 
-mdFoot="<body></html>" 
+mdFoot="</body></html>" 
 
+#patterns to exclude. Anything grep matches will handle this. 
+exclude="index" 
 
 ################################################################################
 function gen
@@ -12,22 +14,32 @@ function gen
     #get the directory listing
     dirList=`ls`
 
-    #echo $mdHead > index.md
+    echo $mdHead > index.md
+    echo >> index.md
+
+    echo \# ${PWD##*/} >> index.md
+    echo >> index.md
 
     #loop over each file/directory and add it to the index. 
 
     for file in $dirList; do 
 	if [ -d $file ]; then 
+	    echo "* [$file]($file)" >> index.md
 	    cd $file
 	    gen
-	    echo "* [$file]($file)" #>> index.md
+	    cd .. 
 	else
-	    htmlFile=`echo $file | sed -e "s/\.md$/.html/"` 
-	    baseName=${file%%.*} 
-	    echo "* [$baseName]($htmlFile)" #>>index.md
+	    #check for excluded files: 
+	    if !(echo $file | grep $exclude > /dev/null); then
+		htmlFile=`echo $file | sed -e "s/\.md$/.html/"` 
+		baseName=${file%%.*} 
+		echo "* [$baseName]($htmlFile)" >>index.md
+	    fi
 	fi
 
     done
+    
+    echo $mdFoot >> index.md
 } 
 
 gen
